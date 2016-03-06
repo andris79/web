1263889378f8b56f77031dcdc756ffcc26403d05
root <root@vmsrv6.cs.local>
2016-03-06 15:39:58 +0300

    fff

--- etc/hello.py
+++ etc/hello.py
@@ -1,7 +1,7 @@
-import multiprocessing
+#import multiprocessing
 bind = "0.0.0.0:8080"
-workers = multiprocessing.cpu_count() * 2 + 1
-pythonpath  = '/home/box/web'
+#workers = multiprocessing.cpu_count() * 2 + 1
+#pythonpath  = '/home/box/web'
 
 #CONFIG = {
 #   'mode': 'wsgi',
